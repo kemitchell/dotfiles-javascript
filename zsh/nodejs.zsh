70b09c367b5ce856106749dfbc085783c50ebfd7
Kyle Mitchell <kyle@kemitchell.com>
2015-09-10 03:25:00 -0700

    Add aliases

--- zsh/nodejs.zsh
+++ zsh/nodejs.zsh
@@ -1,3 +1,10 @@
 export NODE_REPL_HISTORY_FILE=$HOME/.node_history
 export NODE_REPL_HISTORY_SIZE=10000
 export NODE_REPL_MODE=strict
+
+alias nn='node'
+
+alias n='npm'
+alias ni='npm install'
+alias np='npm publish'
+alias nt='npm test'
